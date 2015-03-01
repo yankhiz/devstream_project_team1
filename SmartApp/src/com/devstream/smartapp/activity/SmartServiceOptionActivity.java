c57d98dcb7464f62b97025fef833f0034e77f7ff
Allan <java.allan@yahoo.com>
2015-03-01 10:53:21 +0000

    utilize Sharedpreferences to store authkey

--- SmartApp/src/com/devstream/smartapp/activity/SmartServiceOptionActivity.java
+++ SmartApp/src/com/devstream/smartapp/activity/SmartServiceOptionActivity.java
@@ -17,7 +17,9 @@ import com.devstream.smartapp.R;
 import com.devstream.smartapp.model.Appointment_Service_Option_Model;
 import com.devstream.smartapp.utility.HttpAuthClazz;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
@@ -72,13 +74,15 @@ public class SmartServiceOptionActivity extends ActionBarActivity {
 
 		@Override
 		protected void onPreExecute() {
+			SharedPreferences preferences = getSharedPreferences("credential", Context.MODE_PRIVATE);
+			token = preferences.getString("authToken", "");
 
 		}
 
 		@Override
 		protected String doInBackground(String... params) {
 			Log.d("asynctask", "doInbackground called");
-			token = HttpAuthClazz.getInstance().getAuthKey();
+			//token = HttpAuthClazz.getInstance().getAuthKey();
 			listOfServiceOption = new ArrayList<String>();
 
 			try {
